0c76615acabfb85329b9e41fb1551147d218afb4
HUGO1010133 <82281891+HUGO1010133@users.noreply.github.com>
2021-06-03 12:55:20 -0500

    Add files via upload

---
+++ Sesion_2/Ejercicio_1/ejercicio1.js
@@ -0,0 +1,19 @@
+
+
+var time = 13;
+var saludo;
+
+if (time < 12) {
+    saludo = "Buenos dias";
+}
+
+if (time < 20) {
+    saludo = "Buenas tardes";
+}
+
+if (time >= 20) {
+    saludo = "Buenas noches";
+}
+
+console.log(saludo)
+
